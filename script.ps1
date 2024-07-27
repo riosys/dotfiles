ec3dc5fbabc385ab8bd381bcfe678c5cd608c1f8
Armando Rios <armandorios.dev@proton.me>
2024-07-26 21:21:13 -0700

    🪛 powershell script updated

--- script.ps1
+++ script.ps1
@@ -1,16 +1,49 @@
-# Copiar configuración de Windows Terminal$terminalConfigPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-if (-Not (Test-Path -Path $terminalConfigPath)) {
-    New-Item -ItemType Directory -Force -Path $terminalConfigPath
+# Función para instalar programas
+function Install-Program {
+    param (
+        [string]$programName
+    )
+    winget install --id $programName
 }
 
-Copy-Item -Path "C:\Users\rios\dotfiles\terminal\settings.json" -Destination "$terminalConfigPath\settings.json" -Force
+# Instalar programas necesarios
+Install-Program "Google.Chrome"
+Install-Program "Microsoft.VisualStudioCode"
+Install-Program "Google.Chrome"
+Install-Program "Brave.Brave"
+Install-Program "Microsoft.PowerShell"
+Install-Program "Microsoft.VisualStudioCode"
+Install-Program "Neovim.Neovim"
+Install-Program "JanDeDobbeleer.OhMyPosh"
 
-# Copiar configuración de PowerShell
-$profileDirectory = Split-Path -Path $PROFILE -Parent
-if (-Not (Test-Path -Path $profileDirectory)) {
-    New-Item -ItemType Directory -Force -Path $profileDirectory
+# Lanzar una nueva terminal para ejecutar comandos específicos
+Start-Process "powershell" -ArgumentList "-NoExit", "-Command", {
+    # Ohmyposh font install
+    oh-my-posh font install JetBrainsMono
+
+    # Terminal-Icons install
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+
+    # Copiar configuración de Windows Terminal
+    $terminalConfigPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+    if (-Not (Test-Path -Path $terminalConfigPath)) {
+        New-Item -ItemType Directory -Force -Path $terminalConfigPath
+    }
+
+    Copy-Item -Path "C:\Users\rios\.dotfiles\terminal\settings.json" -Destination "$terminalConfigPath\settings.json" -Force
+
+    # Copiar configuración de PowerShell
+    $profileDirectory = Split-Path -Path $PROFILE -Parent
+    if (-Not (Test-Path -Path $profileDirectory)) {
+        New-Item -ItemType Directory -Force -Path $profileDirectory
+    }
+
+    Copy-Item -Path "C:\Users\rios\.dotfiles\terminal\Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
+
+    Start-Process "powershell" -ArgumentList "-NoExit"
+
+    exit
 }
 
-Copy-Item -Path "C:\Users\rios\dotfiles\terminal\Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
+exit
 
-Write-Output "SetUp finish!!!"
