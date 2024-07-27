4ef330dbeea68a78bfc47250832bae94dbdbd210
Armando Rios <armandorios.dev@proton.me>
2024-07-26 21:24:16 -0700

    🪟 powershell config file

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # OhMyPosh prompt init
-oh-my-posh init pwsh --config ~/dotfiles/Terminal/theme.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~/.dotfiles/Terminal/theme.omp.json | Invoke-Expression
 
 # Termianl icons init
 Import-Module Terminal-Icons
