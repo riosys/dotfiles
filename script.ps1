4f2a0a58ddb9d92efd874ed7124930af853bd635
Armando Rios <armandorios.dev@proton.me>
2024-07-26 22:19:54 -0700

    🪛 script updated

--- script.ps1
+++ script.ps1
@@ -9,41 +9,29 @@ function Install-Program {
 # Instalar programas necesarios
 Install-Program "Google.Chrome"
 Install-Program "Microsoft.VisualStudioCode"
-Install-Program "Google.Chrome"
 Install-Program "Brave.Brave"
 Install-Program "Microsoft.PowerShell"
-Install-Program "Microsoft.VisualStudioCode"
 Install-Program "Neovim.Neovim"
 Install-Program "JanDeDobbeleer.OhMyPosh"
 
 # Lanzar una nueva terminal para ejecutar comandos específicos
-Start-Process "powershell" -ArgumentList "-NoExit", "-Command", {
-    # Ohmyposh font install
-    oh-my-posh font install JetBrainsMono
+Start-Process "powershell" -ArgumentList "-NoExit" -Command `"& {
+    oh-my-posh font install JetBrainsMono;
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Force;
 
-    # Terminal-Icons install
-    Install-Module -Name Terminal-Icons -Repository PSGallery -Force
-
-    # Copiar configuración de Windows Terminal
-    $terminalConfigPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+    $terminalConfigPath = `"$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState`";
     if (-Not (Test-Path -Path $terminalConfigPath)) {
-        New-Item -ItemType Directory -Force -Path $terminalConfigPath
+        New-Item -ItemType Directory -Force -Path $terminalConfigPath;
     }
+    Copy-Item -Path `"C:\Users\rios\.dotfiles\terminal\settings.json`" -Destination `"$terminalConfigPath\settings.json`" -Force;
 
-    Copy-Item -Path "C:\Users\rios\.dotfiles\terminal\settings.json" -Destination "$terminalConfigPath\settings.json" -Force
-
-    # Copiar configuración de PowerShell
-    $profileDirectory = Split-Path -Path $PROFILE -Parent
+    $profileDirectory = Split-Path -Path $PROFILE -Parent;
     if (-Not (Test-Path -Path $profileDirectory)) {
-        New-Item -ItemType Directory -Force -Path $profileDirectory
+        New-Item -ItemType Directory -Force -Path $profileDirectory;
     }
-
-    Copy-Item -Path "C:\Users\rios\.dotfiles\terminal\Microsoft.PowerShell_profile.ps1" -Destination $PROFILE -Force
-
-    Start-Process "powershell" -ArgumentList "-NoExit"
-
+    Copy-Item -Path `"C:\Users\rios\.dotfiles\terminal\Microsoft.PowerShell_profile.ps1`" -Destination $PROFILE -Force;
+    Start-Process `"powershell`" -ArgumentList `"-NoExit`";
     exit
-}
+}`"
 
 exit
-
