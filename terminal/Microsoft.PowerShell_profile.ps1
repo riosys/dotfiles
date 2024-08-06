4b5c11d8f9b63b8d4a9abe987d30f3e751cfdfa9
Armando Rios <armandorios.dev@proton.me>
2024-08-06 07:11:01 -0700

    😒 insignificant changes

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
 # OhMyPosh prompt init
 oh-my-posh init pwsh --config ~/.dotfiles/Terminal/theme.omp.json | Invoke-Expression
 
-# Termianl icons init
+# Terminal icons init
 Import-Module Terminal-Icons
 
 # Short commands
 Set-Alias n nvim
 Set-Alias c code
-Set-Alias lvim 'C:\Users\rios\.local\bin\lvim.ps1'
+# Set-Alias lvim 'C:\Users\rios\.local\bin\lvim.ps1'
