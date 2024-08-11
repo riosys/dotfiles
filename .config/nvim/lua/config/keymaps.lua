5ff942fcdce0eb7c3faa063caf010fd3e4bb431a
Armando Rios <armandorios.dev@proton.me>
2024-08-10 19:31:21 -0700

    teclas para telescope agregadas

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,19 @@
--- keymap  for quit.
-vim.keymap.set('n', '<Space>w', ':w<CR>', { noremap = true, silent = true })
--- keymap for write.
-vim.keymap.set('n', '<Space>q', ':q<CR>', { noremap = true, silent = true })
+local keymap = vim.keymap.set
+
+-- keymap  for write.
+keymap('n', '<Space>w', ':w<CR>', { noremap = true, silent = true })
+-- keymap for quit.
+keymap('n', '<Space>q', ':bd<CR>', { noremap = true, silent = true })
+keymap('n', '<Space>qq', ':exit<CR>', { noremap = true, silent = true})
 -- keymap for write in insert mode.
-vim.keymap.set('i', '<c-s>', '<cmd>w<CR><cmd>echo "Saved"<CR>', { noremap = true, silent = true })
+keymap('i', '<c-s>', '<cmd>w<CR><cmd>echo "Saved"<CR>', { noremap = true, silent = true })
 -- keymap for toggle file explorer
-vim.keymap.set('n', '<c-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) 
+keymap('n', '<c-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- keymap from telescope.
+local telescope = require('telescope.builtin')
+-- find files
+keymap('n', '<Space>ff', telescope.find_files, { noremap = true, silent = true, desc = 'Find files...' })
+-- find contend in files
+keymap('n', '<Space>fc', telescope.live_grep, { noremap = true, silent = true, desc = 'Find contend...'})
+
