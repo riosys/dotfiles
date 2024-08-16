4467a5ef73418921ce809aec1755c942fe59ddde
Armando Rios <armandorios.dev@proton.me>
2024-08-15 20:27:17 -0700

    transparent plugin fixed

--- .config/nvim/lua/plugins/transparent.lua
+++ .config/nvim/lua/plugins/transparent.lua
@@ -1,26 +1,3 @@
-return  {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function()
-       require("toggleterm").setup {
-        size = 20,
-        open_mapping = [[<c-\>]],
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 2,
-        start_in_insert = true,
-        insert_mappings = true,
-        terminal_mappings = true,
-        persist_size = true,
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-          width = 120,
-          height = 30,
-          winblend = 0,
-        },
-        shell = 'pwsh'
-      }
-    end,
+return {
+  "xiyaowong/transparent.nvim"
 }
