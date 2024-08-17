de15d4d37ae05fd603a1e801fb3efeae0d500b5a
Armando Rios <armandorios.dev@proton.me>
2024-08-17 12:16:14 -0700

    remove short commands

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,3 @@ oh-my-posh init pwsh --config ~/.dotfiles/Terminal/theme.omp.json | Invoke-Expre
 
 # Terminal icons init
 Import-Module Terminal-Icons
-
-# Short commands
-Set-Alias n nvim
-Set-Alias c code
-# Set-Alias lvim 'C:\Users\rios\.local\bin\lvim.ps1'
