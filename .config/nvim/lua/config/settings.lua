b5a1ca04f9cd747a25d4ee85f6fbf419254dad21
Armando Rios <armandorios.dev@proton.me>
2024-08-17 13:49:17 -0700

    Nvim-tree close on open file

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -3,4 +3,6 @@ vim.o.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4  
+vim.opt.shiftwidth = 4
+vim.opt.fillchars = { eob = " " }
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "NONE" })
